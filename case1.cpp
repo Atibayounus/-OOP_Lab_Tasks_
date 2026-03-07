4851f9079b58b68182cfcde3d87aab6a66547378
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-03-07 16:35:01 +0500

    used to_string

---
+++ case1.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BankAccount {
+private:
+    string accountHolderName;
+    int accountNumber;
+    double balance;
+
+public:
+    // Default Constructor
+    BankAccount() {
+        accountHolderName = "Unknown";
+        accountNumber = 10000;
+        balance = 0.0;
+    }
+
+    // Parameterized Constructor
+    BankAccount(string name, int accNum, double bal) {
+
+        // initializing safely
+        accountHolderName = "Unknown";
+        accountNumber = 10000;
+        balance = 0.0;
+
+        setName(name);
+        setAccountNumber(accNum);
+        setBalance(bal);
+    }
+
+    // Copy Constructor
+    BankAccount(const BankAccount &obj) {
+
+        accountHolderName = "Unknown";
+        accountNumber = 10000;
+        balance = 0.0;
+
+        setName(obj.getName());
+        setAccountNumber(obj.getAccountNumber());
+        setBalance(obj.getBalance());
+    }
+
+    // Setter Functions with Validation
+
+    void setName(string name) {
+        if (name.empty()) {
+            cout << "Invalid Name" << endl;
+        } 
+        else {
+            accountHolderName = name;
+        }
+    }
+
+ void setAccountNumber(int accNum) {
+
+    string a=to_string(accNum);
+    if(a.length()==5) {
+    accountNumber = accNum;
+    }
+    else {
+        cout << "Invalid Account Number" << endl;
+    }
+}
+
+    void setBalance(double bal) {
+        if (bal < 0) {
+            cout << "Invalid Balance" << endl;
+        } 
+        else {
+            balance = bal;
+        }
+    }
+
+    // Getter Functions
+
+    string getName() const {
+        return accountHolderName;
+    }
+
+    int getAccountNumber() const {
+        return accountNumber;
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    // Display Function
+    void displayAccountInfo() {
+        cout << "Account Holder Name: " << accountHolderName << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+
+    BankAccount acc1;
+
+    acc1.setName("Atiba Dar");
+    acc1.setAccountNumber(02345);  // test case with leading zero
+    acc1.setBalance(6000);
+
+    acc1.displayAccountInfo();
+
+    return 0;
+}
